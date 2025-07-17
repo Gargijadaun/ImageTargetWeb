f07a4900b902589af40f5e431e36516e4565e031
Gargi Jadaun <128454603+Gargijadaun@users.noreply.github.com>
2025-07-17 10:32:05 +0530

    te

--- js/man-ar.js
+++ js/man-ar.js
@@ -184,6 +184,10 @@ document.addEventListener("DOMContentLoaded", () => {
     sceneEl.addEventListener("loaded", () => {
         arSystem = sceneEl.systems["mindar-image-system"];
         document.querySelector('#mainScreen .btn-container').classList.add('show');
+        if (!AR_READY) {
+            arSystem.start();
+            AR_READY = true;
+        }
     });
 });
 // function continueWithoutAR() {
